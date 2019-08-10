dfd79a2036140ad20a904dfe6051693bf07b74f8
sanjayms1999 <53893886+sanjayms1999@users.noreply.github.com>
2019-08-10 11:15:28 +0530

    Add files via upload

---
+++ testcase.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(int argc,char **argv)
+{
+	int testcase,n,i,j,key,flag=-1;
+	{
+		printf("please enter the number of testcase:");
+		scanf("%d",&testcase);
+		for(i=0;i<testcase;i++)
+		{
+			printf("enter the size of the array\n");
+			scanf("%d %d",&n,&key);
+			int a[n];
+			printf("enter the array elements\n");
+			for(j=0;j<n;j++)
+			{
+				scanf("%d",&a[j]);
+			}
+			for(j=0;j<n;j++)
+			{
+				
+			
+				if(a[j]==key)
+				{
+					flag=1;
+				}
+				
+		}
+		if(flag==1)
+		{
+			printf("1\n");
+		}
+		else{
+			printf("-1\n");
+		}
+	}
+}
+}
+
+
+
