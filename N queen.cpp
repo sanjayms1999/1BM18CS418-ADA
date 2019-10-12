fd161da5537c317b5d7309fd53169d0e35930324
sanjayms1999 <53893886+sanjayms1999@users.noreply.github.com>
2019-10-12 10:16:53 +0530

    Update N queen.cpp

--- N queen.cpp
+++ N queen.cpp
@@ -1,39 +1,76 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-bool place(int *x, int k, int i){
-	for(int j=1;j<k;j++){
-		if((x[j] == i) || (abs(x[j]-i) == abs(j-k))) // in the same column or diagonal
-			return false;	
-	}
-	return true;
+int N;
+int board[100][100];
+
+int is_attack(int i,int j)
+{
+    int k,l;
+    for(k=0;k<N;k++)
+    {
+        if((board[i][k] == 1) || (board[k][j] == 1))
+            return 1;
+    }
+
+    for(k=0;k<N;k++)
+    {
+        for(l=0;l<N;l++)
+        {
+            if(((k+l) == (i+j)) || ((k-l) == (i-j)))
+            {
+                if(board[k][l] == 1)
+                    return 1;
+            }
+        }
+    }
+    return 0;
 }
 
-void nQueens(int *x, int k, int n){
-	for(int i=1; i<=n; i++){
-		if(place(x,k,i)){
-			x[k]=i;
-			if(k==n){
-				for(int l=1;l<=n;l++)
-					cout<<x[l]<<"\t";
-				cout<<"\n";
-			}
-			else
-				nQueens(x,k+1,n);
-		}
-	}
+int N_queen(int n)
+{
+    int i,j;
+    //if n is 0, solution found
+    if(n==0)
+        return 1;
+    for(i=0;i<N;i++)
+    {
+        for(j=0;j<N;j++)
+        {
+            if((!is_attack(i,j)) && (board[i][j]!=1))
+            {
+                board[i][j] = 1;
+                if(N_queen(n-1)==1)
+                {
+                    return 1;
+                }
+                board[i][j] = 0;
+            }
+        }
+    }
+    return 0;
 }
 
-int main(){
-	int n;
-	cout<<"Enter the number of queens :";
-	cin>>n;
-	if(n<4){
-		cout<<"No solution exists";
-		return 0;	
-	}
-	int *x = new int[n+1];
-	nQueens(x,1,n);
-	return 0;
+int main()
+{
+	
+    cout<<"Enter the value of N \n";
+    cin>>N;
+
+    int i,j;
+    for(i=0;i<N;i++)
+    {
+        for(j=0;j<N;j++)
+        {
+            board[i][j]=0;
+        }
+    }
+    N_queen(N);
+    for(i=0;i<N;i++)
+    {
+        for(j=0;j<N;j++)
+            cout<<board[i][j]<<" ";
+        cout<<endl;
+    }
+
 }
